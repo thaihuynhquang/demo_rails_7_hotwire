427cad4cc4e74034989f5f519ceb8569ce377a6b
thaihq <thai.huynh8324@gmail.com>
2022-05-17 18:25:02 +0700

    initial commit

---
+++ app/javascript/controllers/application.js
@@ -0,0 +1,11 @@
+import { Application } from "@hotwired/stimulus"
+import Rails from "@rails/ujs"
+
+const application = Application.start()
+
+// Configure Stimulus development experience
+application.debug = false
+window.Stimulus   = application
+Rails.start();
+
+export { application }
