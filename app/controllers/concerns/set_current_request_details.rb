64ef47cee49ee6b80f0346137c4164e84e2d5e10
thaihq <thai.huynh8324@gmail.com>
2022-06-14 15:17:37 +0700

    using concerns

--- app/controllers/concerns/set_current_request_details.rb
+++ app/controllers/concerns/set_current_request_details.rb
@@ -1,9 +1,11 @@
 module SetCurrentRequestDetails
-  extend AcctiveSupport::Concern
+  extend ActiveSupport::Concern
 
   included do
-    before_action do
-      Current.user = current_user if: :user_signed_in?
-    end
+    before_action :set_current_user, if: :user_signed_in?
+  end
+
+  def set_current_user
+    Current.user = current_user
   end
 end
